e2ffaebe825a01d8bda54e2048cc73abe3fef0b0
Robert L Carpenter <robert@robacarp.com>
2017-04-12 17:27:03 -0600

    run lolcatme after downloading

--- lolcatme.sh
+++ lolcatme.sh
@@ -2,5 +2,8 @@
 
 mkdir ~/lolcatme
 cd ~/lolcatme
-brew ls lolcat || brew install lolcat
+brew ls lolcat > /dev/null || brew install lolcat
+
 curl -fsSL 'https://raw.githubusercontent.com/robacarp/lolcatme/master/lolcatme.rb?token=AAMvB1hJekKlwMPqddCdxyIpD1ne41n0ks5Y96StwA%3D%3D' > lolcatme.rb
+
+/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby lolcatme.rb
