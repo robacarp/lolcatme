fc181afeeee3785bfdd40ec833ef4f60f5bb9bed
Robert L Carpenter <robert@robacarp.com>
2017-04-12 16:25:07 -0600

    removing unnecessary shell script

--- lolcatme.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-mkdir ~/lolcatme
-cd ~/lolcatme
-brew install lolcat &
-curl -fsSL 'https://raw.githubusercontent.com/robacarp/lolcatme/master/lolcatme.rb?token=AAMvB1hJekKlwMPqddCdxyIpD1ne41n0ks5Y96StwA%3D%3D' > lolcatme.rb
